8e9100a3d65701122d4e7eabf3b82b375c0a4d0e
Iris <iriskalogirou1@gmail.com>
2024-04-18 22:30:25 +0300

    creating navbar with bootstrap5-

---
+++ simple.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="Css/styles.css">
+</head>
+
+<body>
+    <!-- navbar -->
+    <div style="height:10px; background-color:#27aae1;"></div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a href="#" class="navbar-brand">IRIS_KALOGIROU.COM</a>
+            <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapseCMS">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapseCMS">
+
+                <ul class="navbar-nav mr-auto">
+
+                    <li class="nav-item">
+                        <a href="MyProfile.php" class="nav-link"><i class="fa-solid fa-user text-success"></i> My
+                            Profile</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="Dashboard.php" class="nav-link">Dashboard</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="Posts.php" class="nav-link">Posts</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="Categories.php" class="nav-link">Categories</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="Admins.php" class="nav-link">Manage Admins</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="Comments.php" class="nav-link">Comments</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="Blog.php?page=1" class="nav-link">Live Blog</a>
+                    </li>
+
+
+                </ul>
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item"><a href="Logout.php" class="nav-link"><i
+                                class="fas fa-user-times text-danger"></i>
+                            Logout</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div style="height:10px; background-color:#27aae1;"></div>
+
+
+
+
+    <script src="https://kit.fontawesome.com/e3a678f973.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+
+</body>
+
+</html>
