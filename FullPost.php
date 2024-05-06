edfa1dde9f06b4f6197841960c1968f0d987ddec
Iris <iriskalogirou1@gmail.com>
2024-05-06 17:52:20 +0300

    adding admin manage page

--- FullPost.php
+++ FullPost.php
@@ -59,7 +59,7 @@ if(empty($Name)||empty($Email)||empty($Comment)){
     <title>Blog Page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="Css/styles.css">
+    <link rel="stylesheet" href="public_html/css/styles.css">
 </head>
 
 <body>
@@ -175,11 +175,13 @@ if(empty($Name)||empty($Email)||empty($Comment)){
 
             <!-- FETCH EXISTING COMMENTS  -->
             <div class="container mt-4">
-                <h5 class="mb-3">Comments</h5>
+                <h5 class="mb-3 FieldInfo">Comments</h5>
 
                 <?php 
                 global $ConnectingDB;
-                $sql= "SELECT * FROM comments WHERE post_id='$SearchQueryParameter'";
+                $sql= "SELECT * FROM comments
+                 WHERE post_id='$SearchQueryParameter'
+                 AND status='ON'";
                 $stmt = $ConnectingDB->query($sql);
                 while ($DataRows = $stmt->fetch()){
                     $CommentDate=$DataRows['datetime'];
@@ -187,12 +189,20 @@ if(empty($Name)||empty($Email)||empty($Comment)){
                     $CommentContent=$DataRows['comment'];
                 ?>
                 <div class="card mb-3">
-                    <div class="card-header">
-                        <h6 class="card-title mb-0"><?php echo $CommentAuthor; ?></h6>
-                        <p class="card-text text-muted small"><?php echo $CommentDate; ?></p>
-                    </div>
-                    <div class="card-body">
-                        <p class="card-text"><?php echo $CommentContent; ?></p>
+                    <div class="row g-0">
+                        <div class="col-md-2">
+                            <img class="img-fluid" src="public_html/img/user.png" alt="">
+                        </div>
+                        <div class="col-md-10">
+                            <div class="card-header">
+
+                                <h6 class="card-title mb-0"><?php echo $CommentAuthor; ?></h6>
+                                <p class="card-text text-muted small"><?php echo $CommentDate; ?></p>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text"><?php echo $CommentContent; ?></p>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <?php }?>
