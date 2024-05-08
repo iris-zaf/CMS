62cb98d7f56912cd079e2ee6e1158fa7fcbfbcf0
Iris <iriskalogirou1@gmail.com>
2024-05-08 23:00:51 +0300

    config

--- resources/config.php
+++ resources/config.php
@@ -7,11 +7,19 @@ $config=array(
 defined("INCLUDES_PATH")
 	or define("INCLUDES_PATH", realpath(dirname(__FILE__) . '/../Includes'));
 
-$host = 'localhost';
-$dbname ='cms';
-$user = 'root';
-$pass = '4499';
+    $host = getenv('DB_HOST') ?: 'localhost';
+    $dbname = getenv('DB_NAME') ?: 'cms';
+    $user = getenv('DB_USER') ?: 'root';
+    $pass = getenv('DB_PASS') ?: '4499';
 
+    
+    // Set the database configuration
+$databaseConfig = array(
+    'host' => $host,
+    'dbname' => $dbname,
+    'user' => $user,
+    'pass' => $pass,
+);
 
 ini_set("error_reporting", "true");
 error_reporting(E_ALL|E_STRICT);
